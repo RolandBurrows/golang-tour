257f22e72fef809ff5b940e126e1d7b03d19c40d
Roland Burrows <rolandnburrows@gmail.com>
2015-10-30 23:38:10 -0600

    Add results from Welcome - Page 1

---
+++ 00 - Welcome/Page_01.go
@@ -0,0 +1,15 @@
+// https://tour.golang.org/welcome/1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// -- Results --
+
+// Hello, World!
+
+// Program exited.

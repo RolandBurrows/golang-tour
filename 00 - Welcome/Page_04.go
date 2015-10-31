b68034c31372de47056673abdff6d5480c256c5d
Roland Burrows <rolandnburrows@gmail.com>
2015-10-30 23:44:57 -0600

    Add results from Welcome - Page 4

---
+++ 00 - Welcome/Page_04.go
@@ -0,0 +1,21 @@
+// https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
+
+// -- Results --
+
+// Welcome to the playground!
+// The time is 2009-11-10 23:00:00 +0000 UTC
+
+// Program exited.

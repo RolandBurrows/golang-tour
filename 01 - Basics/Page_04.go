e3bd4b551cc7ff34648bb90a3004727cac607fc5
Roland Burrows <rolandnburrows@gmail.com>
2015-11-01 15:38:08 -0700

    Add results from Basics - page 4

---
+++ 01 - Basics/Page_04.go
@@ -0,0 +1,20 @@
+// https://tour.golang.org/basics/4
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
+
+
+// -- Results --
+
+// 55
+
+// Program exited.

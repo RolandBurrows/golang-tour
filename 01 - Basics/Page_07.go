7df0fe9dc42d8e3fcb43c761d07dea47c8695ced
Roland Burrows <rolandnburrows@gmail.com>
2015-11-02 21:53:48 -0700

    Add pre-answer for Basics - page 7

---
+++ 01 - Basics/Page_07.go
@@ -0,0 +1,23 @@
+// https://tour.golang.org/basics/7
+
+package main
+
+import "fmt"
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
+
+
+// -- Results --
+
+// First guess:
+// x = (17 * 4) / 9 = 7.555bar
+// y = (17) - 7.555bar = 9.444bar
+// print: "7.555, 9.444"

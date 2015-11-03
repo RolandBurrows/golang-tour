48a68e25f6e7c4de6733006627c5b024c79c63b7
Roland Burrows <rolandnburrows@gmail.com>
2015-11-02 21:55:48 -0700

    Add results from Basics - Page 7

--- 01 - Basics/Page_07.go
+++ 01 - Basics/Page_07.go
@@ -21,3 +21,13 @@ func main() {
 // x = (17 * 4) / 9 = 7.555bar
 // y = (17) - 7.555bar = 9.444bar
 // print: "7.555, 9.444"
+
+
+// Run Results:
+
+// 7 10
+
+// Program exited.
+
+
+// Derp. They're ints, not floats.

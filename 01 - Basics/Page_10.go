3a140113ec5e5458617397e0d7467fb8c1bd584c
Roland Burrows <rolandnburrows@gmail.com>
2015-11-03 22:08:31 -0700

    Add results from Basics - page 10

---
+++ 01 - Basics/Page_10.go
@@ -0,0 +1,20 @@
+// https://tour.golang.org/basics/10
+
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
+
+
+// -- Results --
+
+// 1 2 3 true false no!
+
+// Program exited.

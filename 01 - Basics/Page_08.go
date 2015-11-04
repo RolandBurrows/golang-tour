0cd5b190eedcbd1afeea45e7db0a8fd757a77158
Roland Burrows <rolandnburrows@gmail.com>
2015-11-03 21:10:28 -0700

    Add results from Basics - page 8

---
+++ 01 - Basics/Page_08.go
@@ -0,0 +1,19 @@
+// https://tour.golang.org/basics/8
+
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+
+// -- Results --
+
+// 0 false false false
+
+// Program exited.

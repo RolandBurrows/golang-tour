8ec8baebcce757a9941b44db9529c9aeeeee3e31
Roland Burrows <rolandnburrows@gmail.com>
2015-11-03 22:59:18 -0700

    Add results from Basics - page 11

---
+++ 01 - Basics/Page_11.go
@@ -0,0 +1,30 @@
+// https://tour.golang.org/basics/11
+
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	const f = "%T(%v)\n"
+	fmt.Printf(f, ToBe, ToBe)
+	fmt.Printf(f, MaxInt, MaxInt)
+	fmt.Printf(f, z, z)
+}
+
+
+// -- Results --
+
+// bool(false)
+// uint64(18446744073709551615)
+// complex128((2+3i))
+
+// Program exited.

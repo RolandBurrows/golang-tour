c26aaf9a950f80ad474612ca675f7abbee00c816
Roland Burrows <rolandnburrows@gmail.com>
2015-11-06 23:11:49 -0700

    Add results from Flow Control - page 3
    A better implementation of page 2's example

---
+++ 02 - Flow Control/Page_03.go
@@ -0,0 +1,29 @@
+// https://tour.golang.org/flowcontrol/3
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for sum < 1000 {
+		fmt.Println(sum)
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
+
+// -- Results --
+
+// 1
+// 2
+// 4
+// 8
+// 16
+// 32
+// 64
+// 128
+// 256
+// 512
+// 1024

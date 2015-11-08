d80634dbc17d49091c551e12427d647fb36bbf16
Roland Burrows <rolandnburrows@gmail.com>
2015-11-08 12:41:55 -0700

    Init Flow Control page 8 exercise

---
+++ 02 - Flow Control/Page_08.go
@@ -0,0 +1,17 @@
+// https://tour.golang.org/flowcontrol/8
+
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
+
+
+// -- Results --

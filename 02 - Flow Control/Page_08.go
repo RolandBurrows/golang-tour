b15fac1ae6fd50ac135bb3a55c7dce61a7cab19d
Roland Burrows <rolandnburrows@gmail.com>
2015-11-08 19:12:00 -0700

    Complete Flow Control - page 8 exercise
    (Draw the rest of the owl.)

--- 02 - Flow Control/Page_08.go
+++ 02 - Flow Control/Page_08.go
@@ -6,12 +6,43 @@ import (
 	"fmt"
 )
 
+var delta float64 = 0.0000001
+
+func Compare(a, b float64) bool {
+	if ((a - b) < delta && (b - a) < delta) {
+		return true
+	}
+	return false
+}
+
+func Newt(x, z float64) float64 {
+	lim := 1
+	zee := 1.0
+	for lim < 100 {
+		zee = (z-(((z*z)-x)/(2*z)))
+		if Compare(zee, z) {
+			return zee
+		}
+		z = zee
+		lim++
+	}
+	return 0.0
+}
+
 func Sqrt(x float64) float64 {
+	var init float64 = 1
+	return Newt(x, init)
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(10000))
 }
 
 
 // -- Results --
+
+// 1 -> 1
+// 2 -> 1.4142135623730951
+// 4 -> 2.000000000000002		// TODO: Why is this not exactly 2?
+// 25 -> 5
+// 10000 -> 100

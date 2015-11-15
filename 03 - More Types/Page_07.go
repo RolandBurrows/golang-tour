052e11ed2bba60aed2729c558b7503f500f6dc73
Roland Burrows <rolandnburrows@gmail.com>
2015-11-14 19:13:29 -0700

    Add results from More Types - page 7
    [ ] and dice.

---
+++ 03 - More Types/Page_07.go
@@ -0,0 +1,25 @@
+// https://tour.golang.org/moretypes/7
+
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("s ==", s)
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("s[%d] == %d\n", i, s[i])
+	}
+}
+
+
+// -- Results --
+
+// s == [2 3 5 7 11 13]
+// s[0] == 2
+// s[1] == 3
+// s[2] == 5
+// s[3] == 7
+// s[4] == 11
+// s[5] == 13

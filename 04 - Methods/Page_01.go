b3f3dcb5506ee935f5695c11b78ef7daf819fb4f
Roland Burrows <rolandnburrows@gmail.com>
2015-11-21 23:04:14 -0700

    Add results from Methods - page 1
    This language is so classless, it could be a marxist utopia.

---
+++ 04 - Methods/Page_01.go
@@ -0,0 +1,28 @@
+// https://tour.golang.org/methods/1
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
+
+
+// -- Results --
+
+// 5
+
+// Program exited.

d988f39995381438e09c1cfd4c5ba0f36d70eeef
Roland Burrows <rolandnburrows@gmail.com>
2015-11-24 23:30:56 -0700

    Add final results from Methods - page 7
    . .- ... -.--     .--. . .- ... -.--

--- 04 - Methods/Page_07.go
+++ 04 - Methods/Page_07.go
@@ -6,7 +6,9 @@ import "fmt"
 
 type IPAddr [4]byte
 
-// TODO: Add a "String() string" method to IPAddr.
+func (p IPAddr) String() string {
+    return fmt.Sprintf("%v.%v.%v.%v",p[0], p[1], p[2], p[3])
+}
 
 func main() {
 	addrs := map[string]IPAddr{
@@ -21,7 +23,7 @@ func main() {
 
 // -- Results --
 
-// loopback: [127 0 0 1]
-// googleDNS: [8 8 8 8]
+// loopback: 127.0.0.1
+// googleDNS: 8.8.8.8
 
 // Program exited.

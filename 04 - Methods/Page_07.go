709781e3f43f3ae04e63f05e17a6ca5bacded620
Roland Burrows <rolandnburrows@gmail.com>
2015-11-24 22:55:06 -0700

    Init Methods - page 7
    The Google Public DNS handles 400 billion requests per day. Localhost
    usage statistics is harder to come by.

---
+++ 04 - Methods/Page_07.go
@@ -0,0 +1,27 @@
+// https://tour.golang.org/methods/7
+
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+
+func main() {
+	addrs := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for n, a := range addrs {
+		fmt.Printf("%v: %v\n", n, a)
+	}
+}
+
+
+// -- Results --
+
+// loopback: [127 0 0 1]
+// googleDNS: [8 8 8 8]
+
+// Program exited.

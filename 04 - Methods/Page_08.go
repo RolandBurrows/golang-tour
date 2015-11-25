fdde407f9c126ffc5a3bd249e7ca4249c20c2930
Roland Burrows <rolandnburrows@gmail.com>
2015-11-24 23:34:42 -0700

    Add results from Methods - page 8
    TODO: Bookmark and tinker!

---
+++ 04 - Methods/Page_08.go
@@ -0,0 +1,38 @@
+// https://tour.golang.org/methods/8
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+func run() error {
+	return &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+
+// -- Results --
+
+// at 2009-11-10 23:00:00 +0000 UTC, it didn't work
+
+// Program exited.

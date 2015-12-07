e7e3b53bdad00aa10b59ac5bce546bc4eb6404d3
Roland Burrows <rolandnburrows@gmail.com>
2015-12-06 23:30:01 -0700

    Implement Methods - page 12
    TODO: See if it can be implemented via:
    https://golang.org/pkg/container/ring/

--- 04 - Methods/Page_12.go
+++ 04 - Methods/Page_12.go
@@ -12,16 +12,26 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func (rotate *rot13Reader) Read(input []byte) (n int, err error) {
+	n,err = rotate.r.Read(input)
+	for i := 0; i < len(input); i++ {
+		if (input[i] >= 'A' && input[i] < 'N') || (input[i] >='a' && input[i] < 'n') {
+			input[i] += 13
+		} else if (input[i] > 'M' && input[i] <= 'Z') || (input[i] > 'm' && input[i] <= 'z'){
+			input[i] -= 13
+		}
+	}
+	return
+}
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
-	io.Copy(os.Stdout, &r)							// Failure point
+	io.Copy(os.Stdout, &r)
 }
 
 
 // -- Results --
 
-// prog.go:16: cannot use &r (type *rot13Reader) as type io.Reader in argument to io.Copy:
-// 	*rot13Reader does not implement io.Reader (missing Read method)
-
+// You cracked the code!
 // Program exited.

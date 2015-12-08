cfacd7cfd906e8ebe9b6ec16ade275e8bc713ab8
Roland Burrows <rolandnburrows@gmail.com>
2015-12-07 22:43:30 -0700

    Add results from Methods - page 13
    Results may vary.

---
+++ 04 - Methods/Page_13.go
@@ -0,0 +1,32 @@
+// https://tour.golang.org/methods/13
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, "Hello!")
+}
+
+func main() {
+	var h Hello
+	err := http.ListenAndServe("localhost:4000", h)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+// -- Results --
+
+// 2009/11/10 23:00:00 listen tcp: Protocol not available
+
+// Program exited.

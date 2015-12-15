694e7952ab7f3c03d186705a77badf9d2c0d9e5b
Roland Burrows <rolandnburrows@gmail.com>
2015-12-14 23:14:02 -0700

    Add results for Methods - page 14
    Neat.

--- 04 - Methods/Page_14.go
+++ 04 - Methods/Page_14.go
@@ -3,39 +3,45 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"fmt"
+        "fmt"
+        "log"
+        "net/http"
 )
 
 type String string
 
+func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, s)
+}
+
 type Struct struct {
-	Greeting string
-	Punct string
-	Who string
+        Greeting string
+        Punct    string
+        Who      string
 }
 
-func (s String) ServeHTTP(
-	w http.ResponseWriter,
-	r *http.Request) {
-	fmt.Fprint(w, s)
-}
-
-func (s Struct) ServeHTTP(
-	w http.ResponseWriter,
-	r *http.Request) {
-	fmt.Fprint(w, s)
+func (s *Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "%s%s %s", s.Greeting, s.Punct, s.Who)
 }
 
 func main() {
-	http.Handle("/string", String("I'm a frayed knot."))
-	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
-	log.Fatal(http.ListenAndServe("localhost:4000", nil))
-}
+        http.Handle("/string", String("I'm a frayed knot."))
+        http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+        err := http.ListenAndServe("localhost:4000", nil)
+        if err != nil {
+                log.Fatal(err)
+        }
+}  
 
 
 // -- Results --
 
-// // view-source:http://localhost:4000/
+// // view-source:http://localhost:4000
 // 404 page not found
+
+// // view-source:http://localhost:4000/string
+// I'm a frayed knot.
+
+// // view-source:http://localhost:4000/struct
+// {Hello : Gophers!}
+

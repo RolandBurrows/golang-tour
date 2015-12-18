1b93dbbd0f7d4724c2c50608cdae53c1b5d06a90
Roland Burrows <rolandnburrows@gmail.com>
2015-12-17 22:37:08 -0700

    Add results from Concurrency - page 2
    Hmm. Warrants further study. A practical application example would be
    nice.

---
+++ 05 - Concurrency/Page_02.go
@@ -0,0 +1,33 @@
+// https://tour.golang.org/concurrency/2
+
+// Channels
+
+package main
+
+import "fmt"
+
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
+
+
+// -- Results --
+
+// -5 17 12
+
+// Program exited.

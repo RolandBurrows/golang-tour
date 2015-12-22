5f5e773f3e8ef4bf5060daafc70a2c11883ce9a1
Roland Burrows <rolandnburrows@gmail.com>
2015-12-21 23:37:47 -0700

    Init Concurrency page 7+8 exercise
    Golang needs an 'auto-implement' feature. (trollface)

---
+++ 05 - Concurrency/Page_07.go
@@ -0,0 +1,27 @@
+// https://tour.golang.org/concurrency/7
+// https://tour.golang.org/concurrency/8
+
+// Exercise: Equivalent Binary Trees
+
+package main
+
+import "golang.org/x/tour/tree"
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int)
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool
+
+func main() {
+}
+
+
+// -- Results --
+
+// prog.go:7: missing function body for "Walk"
+// prog.go:11: missing function body for "Same"
+
+// Program exited.

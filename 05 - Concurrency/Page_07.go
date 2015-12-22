2f6ef896cdab88781b731cf8c46305523d088c9e
Roland Burrows <rolandnburrows@gmail.com>
2015-12-21 23:52:47 -0700

    Analyze engineers' binary trees 2015 solution
    Goroutine leak welded shut with a nifty diff.

--- 05 - Concurrency/Page_07.go
+++ 05 - Concurrency/Page_07.go
@@ -10,25 +10,30 @@ import (
         "golang.org/x/tour/tree"
 )
 
-func walkImpl(t *tree.Tree, ch chan int) {
+func walkImpl(t *tree.Tree, ch, quit chan int) {
         if t == nil {
                 return
         }
-        walkImpl(t.Left, ch)
-        ch <- t.Value
-        walkImpl(t.Right, ch)
+        walkImpl(t.Left, ch, quit)
+        select {
+        case ch <- t.Value:
+        case <-quit: return
+        }
+        walkImpl(t.Right, ch, quit)
 }
 
-func Walk(t *tree.Tree, ch chan int) {
-        walkImpl(t, ch)
+func Walk(t *tree.Tree, ch, quit chan int) {
+        walkImpl(t, ch, quit)
         close(ch)
 }
 
 func Same(t1, t2 *tree.Tree) bool {
         w1, w2 := make(chan int), make(chan int)
+        quit := make(chan int)
+        defer close(quit)
 
-        go Walk(t1, w1)
-        go Walk(t2, w2)
+        go Walk(t1, w1, quit)
+        go Walk(t2, w2, quit)
 
         for {
                 v1, ok1 := <-w1

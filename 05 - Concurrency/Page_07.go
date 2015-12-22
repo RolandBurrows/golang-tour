6f5f2fd2b5c4c5409ad54cdae178492f4aaab013
Roland Burrows <rolandnburrows@gmail.com>
2015-12-21 23:48:51 -0700

    Analyze engineers' binary trees 2012 solution
    Herald of Doom: (NOTE: The implementation leaks goroutines when trees
    are different.)

--- 05 - Concurrency/Page_07.go
+++ 05 - Concurrency/Page_07.go
@@ -5,23 +5,63 @@
 
 package main
 
-import "golang.org/x/tour/tree"
+import (
+        "fmt"
+        "golang.org/x/tour/tree"
+)
 
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func walkImpl(t *tree.Tree, ch chan int) {
+        if t == nil {
+                return
+        }
+        walkImpl(t.Left, ch)
+        ch <- t.Value
+        walkImpl(t.Right, ch)
+}
 
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Walk(t *tree.Tree, ch chan int) {
+        walkImpl(t, ch)
+        close(ch)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+        w1, w2 := make(chan int), make(chan int)
+
+        go Walk(t1, w1)
+        go Walk(t2, w2)
+
+        for {
+                v1, ok1 := <-w1
+                v2, ok2 := <-w2
+                if !ok1 || !ok2 {
+                        return ok1 == ok2
+                }
+                if v1 != v2 {
+                        return false
+                }
+        }
+}
 
 func main() {
+        fmt.Print("tree.New(1) == tree.New(1): ")
+        if Same(tree.New(1), tree.New(1)) {
+                fmt.Println("PASSED")
+        } else {
+                fmt.Println("FAILED")
+        }
+
+        fmt.Print("tree.New(1) != tree.New(2): ")
+        if !Same(tree.New(1), tree.New(2)) {
+                fmt.Println("PASSED")
+        } else {
+                fmt.Println("FAILED")
+        }
 }
 
 
 // -- Results --
 
-// prog.go:7: missing function body for "Walk"
-// prog.go:11: missing function body for "Same"
+// tree.New(1) == tree.New(1): PASSED
+// tree.New(1) != tree.New(2): PASSED
 
 // Program exited.

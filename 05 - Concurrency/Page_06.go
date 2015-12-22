34dbb5fdaaa8627bdf86e1e3a7321fff2cb64fc5
Roland Burrows <rolandnburrows@gmail.com>
2015-12-21 23:31:53 -0700

    Add results from Concurrency - page 6
    Okay, this example is one of their cleverest and straightforward by far.

---
+++ 05 - Concurrency/Page_06.go
@@ -0,0 +1,46 @@
+// https://tour.golang.org/concurrency/6
+
+// Default Selection
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
+
+// -- Results --
+
+//     .
+//     .
+// tick.
+//     .
+//     .
+// tick.
+//     .
+//     .
+// tick.
+//     .
+//     .
+// tick.
+//     .
+//     .
+// BOOM!
